c59f8d51343eac84cc1b167b7c1ac83b7ed35e9c
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-02-16 11:30:55 -0500

    Initial commit with a project already started

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Diego José Saravia</title>
+    <link rel="stylesheet" href="css/index.css">
+
+    <link rel="shortcut icon" href="media/favicon.png" type="image/x-icon">
+</head>
+<body>
+    <main>
+        <div class="section-3-bg">
+            <img src="media/images/section-3-bj.jpg" alt="Background image">
+        </div>
+        <div id="fullpage">
+            <div class="section bg-b" id="section-1" data-anchor="presentation">
+                <div class="content" id="particles">
+                    <div class="title">
+                        <h1>Programador web</h1>
+                    </div>
+                    <div class="img">
+                        <div class="container"><img src="media/images/presentation-img.png" alt="My face :p"></div>
+                    </div>
+                    <div class="go-down-btn">
+                        <div class="text hover">Discover more about me</div>
+                        <div class="icon hover"><img src="media/images/arrow-down.png"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="section" id="section-2" data-anchor="about">
+                <div class="content" style="background-color: #fff;">
+                    <div class="return-flag r b"></div>
+                    <h2 class="subtitle-2">¿Quién soy yo?</h2>
+                    <div class="content">
+                        <p class="description">
+                            Soy Diego José Saravia, Web Developer desde que tengo 15 años.
+                            Soy una persona muy extrovertida, hablantina y proactiva que le gusta aprender cosas nuevas todos los días =D. Hago código por diversión e invento cosas nuevas en mi tiempo libre. Soy amante de varios tipos de música, la magia, la matemática, de la fotografía, los retos y los animales cuadraros...
+                        </p>
+                        <div class="image">
+                            <img src="media/images/Logo.png" alt="Logo">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section" id="section-3" data-anchor="abilities">
+                <div class="content"  style="color: #fff;">
+                    <div class="bg-opacity"></div>
+                    <div class="return-flag l w"></div>
+                    <h2 class="subtitle-2">Conocimientos</h2>
+                    <div class="spheres">
+                        <div class="spheres-row">
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                            <div class="sphere"><img src="media/images/icons/php-icon.png" alt="php icon"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section" data-anchor="slide4">4</div>
+        </div>
+    </main>
+    <script src="js/fullpage.min.js"></script>
+    <script src="js/particles.min.js"></script>
+    <script src="js/index.js"></script>
+    <script>
+        // document.addEventListener('DOMContentLoaded', function(e) {
+        //     particlesJS.load('particles', 'assets/particles-config.json', function() {
+        //         console.log('Particles loaded');
+        //     });
+        // });
+    </script>
+</body>
+</html>
